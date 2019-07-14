b11f2846c6ac3a752c4db7c1e0fafc27e3ed122e
Mike Jones <mwj12@case.edu>
2019-07-14 15:16:58 -0400

    find smallest index on empty array

---
+++ src/sort/merge/main.py
@@ -0,0 +1,10 @@
+import typing
+
+Number = typing.Union[int, float, "decimal.Decimal"]
+
+def find_smallest_index(arr: typing.List[Number]) -> int:
+    """
+    :param arr: Find the smallest in this list
+    """
+    return None 
+    
