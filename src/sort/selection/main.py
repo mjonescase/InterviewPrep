49db1c47265f52b8d877dd61cee2aa430ccf18ce
Mike Jones <mwj12@case.edu>
2019-07-14 18:21:04 -0400

    rename to selection, get smallest index helper function working

---
+++ src/sort/selection/main.py
@@ -0,0 +1,19 @@
+import sys
+import typing
+
+Number = typing.Union[int, float, "decimal.Decimal"]
+
+
+def find_smallest_index(arr: typing.List[Number]) -> int:
+    """
+    :param arr: Find the smallest in this list
+    """
+    smallest_index: int = None
+    smallest_value: Number = sys.maxsize
+    for index, value in enumerate(arr):
+        if value < smallest_value:
+            smallest_value = value
+            smallest_index = index
+
+    return smallest_index
+    
