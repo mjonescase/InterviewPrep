b92276ef056ef99990787035f2dd23fe530bcc09
Mike Jones <mwj12@case.edu>
2019-07-17 19:07:44 -0400

    get end-to-end tests passing for selection sort

--- src/sort/selection/_end_to_end_test.py
+++ src/sort/selection/_end_to_end_test.py
@@ -4,11 +4,12 @@ from . import main
 
 
 def assert_sorts_as_expected(example: list, expected: list) -> None:
-    assert_that(main.sort(example), equal_to(expected))
+    main.sort(example)
+    assert_that(example, equal_to(expected))
 
 
 def test_sort_simple_example():
-    assert_sorts_as_expected([3, 1, 7, 5], [1, 3, 7, 5])
+    assert_sorts_as_expected([3, 1, 7, 5], [1, 3, 5, 7])
 
 
 def test_sort_empty_list():
