7e4a2284d0ec584183a48666cf7fa0ab9511f1c4
Mike Jones <mwj12@case.edu>
2019-07-22 13:22:37 -0400

    test heapsort ith_child index bound check

--- src/sort/heap/_unit_test.py
+++ src/sort/heap/_unit_test.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 from hamcrest import assert_that, calling, equal_to, raises
 
 from . import main
@@ -13,3 +15,10 @@ def test_parent_ary_lt_0():
 
 def test_parent_happy():
     assert_that(main.parent(1, 2), equal_to(0))
+
+
+def test_ith_child_index_lt_0():
+    assert_that(
+        calling(main.ith_child).with_args(-1, MagicMock(), MagicMock()),
+        raises(AssertionError),
+    )
