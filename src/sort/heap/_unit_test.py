f34f51c394e68d2f33045fa57c6f330be9f6e46f
Mike Jones <mwj12@case.edu>
2019-07-22 13:13:46 -0400

    test bounds check ary on heap.parent

--- src/sort/heap/_unit_test.py
+++ src/sort/heap/_unit_test.py
@@ -5,3 +5,7 @@ from . import main
 
 def test_parent_index_lt_0():
     assert_that(calling(main.parent).with_args(-5, 2), raises(AssertionError))
+
+
+def test_parent_ary_lt_0():
+    assert_that(calling(main.parent).with_args(3, -3), raises(AssertionError))
