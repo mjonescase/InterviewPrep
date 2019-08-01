35d84e7b65003a44158666e9d395ddaf026eb261
Mike Jones <mjones@medasync.com>
2019-08-01 11:09:46 -0400

    install pre-commit hook, lint files on lenovo properly

--- src/hashtable/_unit_test.py
+++ src/hashtable/_unit_test.py
@@ -3,11 +3,13 @@ from hamcrest import assert_that, calling, greater_than_or_equal_to, less_than, 
 from .HashTable import HashTable
 from .HashTableFullException import HashTableFullException
 
+
 def test_hash_none():
     table = HashTable(5)
     assert_that(calling(table.insert).with_args(None), raises(ValueError))
 
+
 def test_hash_happy():
     table = HashTable(5)
-    _ = [ table.insert(i) for i in range(5) ]
+    _ = [table.insert(i) for i in range(5)]
     assert_that(calling(table.insert).with_args(6), raises(HashTableFullException))
